d8541027bba7a2dffc4c8247ddb386831356bcc5
ektaagrawal29 <45243729+ektaagrawal29@users.noreply.github.com>
2019-02-07 15:12:04 -0800

    Update brigade.js

--- test-brigade/brigade.js
+++ test-brigade/brigade.js
@@ -1,53 +1,12 @@
-const { events, Job, Group} = require("brigadier")
+console.log("hello world")
+project_id = "brigade-9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c"
+const { events, Job } = require("brigadier")
 
-events.on("push", function(e, project) {
-  console.log("===> Building " + project.repo.cloneURL + " " + e.commit)
-
-  var node = new Job("node-runner")
-  node.image = "node:8"
-  node.tasks = [
-    "cd /src/hello",
-    "npm install",
-    "node index.js"
-  ]
-  node.env = {
-    "unused": "myval",
-    "unused2": project.secrets.dbPassword || "empty"
-  }
-  node.run()
+events.on("exec", () => {
+    var job = new Job("test-ml-job", "ektaagrawal29/mlapp")
+    //job.resourceRequests.memory = "2Gi";
+    //job.resourceRequests.cpu = "500m";
+    //job.resourceLimits.memory = "3Gi";
+    //job.resourceLimits.cpu = "1";
+    job.run()
 })
-
-events.on("imagePush", function(e) {
-  console.log(JSON.stringify(e))
-  var hook = JSON.parse(e.payload)
-  console.log("===> Image push " + hook.repository.name + ":" + hook.push_data.tag)
-})
-
-events.on("after", function(e, project) {
-  var c = e.cause.event
-  var m = "Hook " + c.type + " is in state " + e.cause.trigger +
-    " for build " + e.commit + " of " + project.repo.name
-
-  if (project.secrets.SLACK_WEBHOOK) {
-    var slack = new Job("slack-notify")
-
-    slack.image = "technosophos/slack-notify:latest"
-    slack.env = {
-      SLACK_WEBHOOK: project.secrets.SLACK_WEBHOOK,
-      SLACK_USERNAME: "AcidBot",
-      SLACK_TITLE: "Build " + e.cause.trigger,
-      SLACK_MESSAGE: m + " <https://" + project.repo.name + ">",
-      SLACK_COLOR: "#00ff00"
-    }
-
-    slack.tasks = ["/slack-notify"]
-
-    if (e.cause.trigger != "success") {
-      slack.env.SLACK_COLOR = "#ff0000"
-    }
-    slack.run()
-  } else {
-    console.log(m)
-  }
-})
-
